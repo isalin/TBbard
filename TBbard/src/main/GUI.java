bbe4b8b89d590751fd034a89494bb341596230e4
rain288 <angelos.h@hotmail.com>
2018-01-31 15:11:12 +0000

    Added: * Scroll bar to the text box * Stop button to stop the playback Fixed: * Adapted the utility to work on the new Bard Performance system * Made the GUI not freeze when there is playback to enable the use of the Stop button to stop the playback. Removed: * Any methods related to mouse input and location

--- TBbard/src/main/GUI.java
+++ TBbard/src/main/GUI.java
@@ -10,6 +10,8 @@ import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetDropEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.util.List;
 
@@ -22,13 +24,19 @@ public class GUI {
 
 	Notes n;
 	MidiParser midi;
+	/** Field to hold the keybind that should stop the playback
+	 * TODO Make this configurable by the user
+	 */
+	int stopPlayback = KeyEvent.VK_ESCAPE;
 
 	public GUI() {
 		JFrame frame = new JFrame();
 
 		JPanel pnPanel0;
 		JButton btPlayButton;
+		JButton btStopButton; // Button to stop the performance
 		JTextArea taText;
+		JScrollPane spText;
 
 		JPanel pnPanel1;
 		JLabel lbCd;
@@ -46,7 +54,7 @@ public class GUI {
 		btPlayButton = new JButton( "Play"  );
 		gbcPanel0.gridx = 0;
 		gbcPanel0.gridy = 18;
-		gbcPanel0.gridwidth = 20;
+		gbcPanel0.gridwidth = 10;
 		gbcPanel0.gridheight = 2;
 		gbcPanel0.fill = GridBagConstraints.BOTH;
 		gbcPanel0.weightx = 1;
@@ -54,8 +62,22 @@ public class GUI {
 		gbcPanel0.anchor = GridBagConstraints.NORTH;
 		gbPanel0.setConstraints( btPlayButton, gbcPanel0 );
 		pnPanel0.add( btPlayButton );
+		
+		btStopButton = new JButton( "Stop"  );
+		gbcPanel0.gridx = 10;
+		gbcPanel0.gridy = 18;
+		gbcPanel0.gridwidth = 10;
+		gbcPanel0.gridheight = 2;
+		gbcPanel0.fill = GridBagConstraints.BOTH;
+		gbcPanel0.weightx = 1;
+		gbcPanel0.weighty = 0;
+		gbcPanel0.anchor = GridBagConstraints.NORTH;
+		gbPanel0.setConstraints( btStopButton, gbcPanel0 );
+		pnPanel0.add( btStopButton );
 
 		taText = new JTextArea(2,10);
+		
+		spText = new JScrollPane( taText );
 		gbcPanel0.gridx = 0;
 		gbcPanel0.gridy = 6;
 		gbcPanel0.gridwidth = 20;
@@ -64,8 +86,9 @@ public class GUI {
 		gbcPanel0.weightx = 1;
 		gbcPanel0.weighty = 1;
 		gbcPanel0.anchor = GridBagConstraints.NORTH;
-		gbPanel0.setConstraints( taText, gbcPanel0 );
-		pnPanel0.add( taText );
+		gbPanel0.setConstraints( spText, gbcPanel0 );
+		spText.setVerticalScrollBarPolicy( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
+		pnPanel0.add( spText );
 
 		pnPanel1 = new JPanel();
 		pnPanel1.setBorder( BorderFactory.createTitledBorder( "Settings" ) );
@@ -206,27 +229,43 @@ public class GUI {
 		{
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				try {
-					Thread.sleep((int)spnDelaySpinner.getValue());
-					n = new Notes((int) spnFpsSpinner.getValue());
-					Notes.waitMultiplier = (double) spnCd.getValue();
+				new Thread() { // Making a new thread instead of sleeping the old one
+					@Override
+					public void run() {
+						try {
+							Thread.sleep((int) spnDelaySpinner.getValue());
+							n = new Notes((int) spnFpsSpinner.getValue());
+							Notes.waitMultiplier = (double) spnCd.getValue();
 
 
-					String text = taText.getText().replace("[", "(").replace("]",")").replace(",","");
-					n.parseSlowdownConstant(text);
+							String text = taText.getText().replace("[", "(").replace("]",")").replace(",","");
+							n.parseSlowdownConstant(text);
 
 
 
-					for (String line : text.split("\\n")){
-						for (String l : line.split(" ")){
-							n.play(l);
+							for (String line : text.split("\\n")){
+								for (String l : line.split(" ")){
+									n.play(l);
+								}
+							}
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
 						}
-					};
-
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+						
+					}
+				}.start(); // We're using a new thread to be able to access Stop still.
+			}
+		});
+		
+		/**
+		 * Action Listener for the stop button
+		 */
+		btStopButton.addActionListener( new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				n.running = false;
 			}
 		});
 
